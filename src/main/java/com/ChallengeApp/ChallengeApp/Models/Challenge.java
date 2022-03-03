89a3420ce9d9df86fd5cc047a4973a8d1987de9b
MeliMano <melimanoni@gmail.com>
2022-03-03 11:28:18 +0100

    db created

--- src/main/java/com/ChallengeApp/ChallengeApp/Models/Challenge.java
+++ src/main/java/com/ChallengeApp/ChallengeApp/Models/Challenge.java
@@ -1,10 +1,11 @@
 package com.ChallengeApp.ChallengeApp.Models;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+@Entity
 public class Challenge {
 
 @Id
--- src/main/java/com/ChallengeApp/ChallengeApp/Models/Question.java
+++ src/main/java/com/ChallengeApp/ChallengeApp/Models/Question.java
@@ -1,6 +1,14 @@
 package com.ChallengeApp.ChallengeApp.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String imgUrl;
     private String challengeQuestion;
