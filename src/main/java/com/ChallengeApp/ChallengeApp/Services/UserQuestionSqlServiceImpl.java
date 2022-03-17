b84b8e6381b3d021354b787584c218c8233f23e0
Carruor <fotoes10@gmail.com>
2022-03-17 19:59:41 +0100

    QuestionListResponseDTO service implemented

--- src/main/java/com/ChallengeApp/ChallengeApp/Services/UserQuestionSqlServiceImpl.java
+++ src/main/java/com/ChallengeApp/ChallengeApp/Services/UserQuestionSqlServiceImpl.java
@@ -4,12 +4,9 @@ package com.ChallengeApp.ChallengeApp.Services;
 import com.ChallengeApp.ChallengeApp.Models.Challenge;
 import com.ChallengeApp.ChallengeApp.Models.User;
 import com.ChallengeApp.ChallengeApp.Models.UserQuestion;
-import com.ChallengeApp.ChallengeApp.Repositories.ChallengeAppRepository;
-import com.ChallengeApp.ChallengeApp.Repositories.QuestionRepository;
 import com.ChallengeApp.ChallengeApp.Repositories.UserQuestionRepository;
 import com.ChallengeApp.ChallengeApp.dtos.QuestionListResponseDTO;
-import com.ChallengeApp.ChallengeApp.dtos.UserQuestionResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -28,18 +25,7 @@ public class UserQuestionSqlServiceImpl implements UserQuestionService {
     public QuestionListResponseDTO getAllfindAllByUserAndQuestion_Challenge(User user, Challenge challenge){
         List<UserQuestion> userQuestions= userQuestionRepository.findAllByUserAndQuestion_Challenge(user, challenge);
         QuestionListResponseDTO questionListResponseDTO = new QuestionListResponseDTO();
-
-        //List<UserQuestionResponseDTO> userQuestionDTO = new ArrayList<UserQuestionResponseDTO>();
-
-        /*for(UserQuestion userQuestion : userQuestions){
-            UserQuestionResponseDTO userQuestionResponseDTO = new UserQuestionResponseDTO();
-            userQuestionResponseDTO.mapFromUserQuestion(userQuestion);
-            *//*userQuestionResponseDTO.setUserId(userQuestion.getId());
-            userQuestionResponseDTO.setQuestionId(userQuestion.getQuestion().getId());
-            userQuestionResponseDTO.setChallengeAnswerId(userQuestion.getChallengeAnswer().getId());
-            userQuestionResponseDTO.setChallengeId(userQuestion.getChallengeAnswer().getQuestion().getChallenge().getId());*//*
-            userQuestionDTO.add(userQuestionResponseDTO);
-        }*/
+        questionListResponseDTO.mapFromQuestionsList(userQuestions);
         return questionListResponseDTO;
     }
 }
